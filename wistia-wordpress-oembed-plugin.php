6d96e46a77973efb0db1aed447baeaabc63fd571
Morgan Estes <morgan.estes@gmail.com>
2013-03-25 14:22:49 -0500

    Update wistia-wordpress-oembed-plugin.php
    Updated oEmbed endpoint and provider regex to the version recommended at http://wistia.com/doc/oembed.

--- wistia-wordpress-oembed-plugin.php
+++ wistia-wordpress-oembed-plugin.php
@@ -9,7 +9,7 @@ Author URI: http://wistia.com
 License: MIT
 */
 
-wp_oembed_add_provider( 'http://*.wistia.com/embed/*', 'http://app.wistia.com/embed/oembed' );
+wp_oembed_add_provider( 'https?:\/\/(.+)?(wistia\.com|wi\.st)\/.*', 'http://fast.wistia.com/oembed' );
 require('wistia-anti-mangler.php');
 
 global $wistia_anti_mangler;
