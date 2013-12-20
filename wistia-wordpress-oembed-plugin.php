e0bf91793a3bf0aa86e8d7605323648286f48775
Max Schnur <mschnur@wistia.com>
2013-12-20 11:13:53 -0500

    Update plugin metadata to 0.7

--- wistia-wordpress-oembed-plugin.php
+++ wistia-wordpress-oembed-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Wistia WordPress Plugin
 Plugin URI: https://github.com/wistia/wistia-wordpress-plugin
 Description: A plugin that allows you to embed videos from your Wistia account into WordPress.
-Version: 0.6
+Version: 0.7
 Author: Wistia, Inc.
 Author URI: http://wistia.com
 License: MIT
