161ff0998f9f4cad4afde59f37e95dd707c9b1cb
Morgan Estes <morgan@golftailor.com>
2014-01-23 11:45:28 -0600

    Merge with Wistia repository.

---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Wistia WordPress Plugin ===
+Contributors: wistia
+Tags: wistia, oembed, video, embed
+Requires at least: 2.9.1
+Tested up to: 3.5.1
+Stable tag: 0.7
+
+Enables all Wistia embed types to be used in your WordPress blog.
+
+== Description ==
+
+Wistia's embed codes are designed to be very durable, but WordPress has a
+history of being particularly troublesome. This plugin transparently makes
+sure that your Wistia embeds will work, no matter what you do.
+
+As of version 0.6 of this plugin, it is recommended that you check
+"Use oEmbed?" under Advanced Options when generating your embed code.
+
+See the Wistia documentation for more:
+http://wistia.com/doc/wordpress#using_the_oembed_embed_code
+
+== Installation ==
+
+1. Make a 'wistia-wordpress-oembed-plugin' directory in '/wp-content/plugins/'.
+2. Upload 'wistia-wordpress-oembed-plugin.php' to the
+'/wp-content/plugins/wistia-wordpress-oembed-plugin/' directory.
+3. Upload 'wistia-anti-mangler.php' to the
+'/wp-content/plugins/wistia-wordpress-oembed-plugin/' directory.
+4. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+
+= 0.7 =
+* Try to fix an issue with string concatenation in certain environemnts.
+
+= 0.6 =
+* Changed oembed regexp to properly detect new Wistia oembed URLs.
+
+= 0.5.1 =
+* Fixed a debug error complaining about `extended_valid_elements`
+undefined in `add_valid_tiny_mce_elements`
+
+= 0.5 =
+* Updated the oembed endpoint for Wistia
+* Updated the regexes for matching Wistia video URLs to the latest recommended in the doc
+
+= 0.4 =
+* Added support for the new Playlist embed structure
+
+= 0.3 =
+* Added support for all SuperEmbed style Wistia embeds
+
+= 0.2 =
+* Added support for SuperEmbed style oEmbeds
+
+= 0.1 =
+* Initial release
---
+++ wistia-wordpress-oembed-plugin.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: Wistia WordPress Plugin
+Plugin URI: https://github.com/wistia/wistia-wordpress-plugin
+Description: A plugin that allows you to embed videos from your Wistia account into WordPress.
+Version: 0.7
+Author: Wistia, Inc.
+Author URI: http://wistia.com
+License: MIT
+*/
+
+wp_oembed_add_provider( '/https?\:\/\/(.+)?(wistia\.com|wi\.st)\/.*/', 'https://fast.wistia.com/oembed', true );
+require('wistia-anti-mangler.php');
+
+global $wistia_anti_mangler;
+$wistia_anti_mangler = new WistiaAntiMangler();
+
+function wistia_extract_embeds($text) {
+  global $wistia_anti_mangler;
+  return $wistia_anti_mangler->extract_embeds($text);
+}
+
+function wistia_insert_embeds($text) {
+  global $wistia_anti_mangler;
+  return $wistia_anti_mangler->insert_embeds($text);
+}
+
+function wistia_insert_embeds_for_editor($text) {
+  global $wistia_anti_mangler;
+  return $wistia_anti_mangler->insert_embeds_for_editor($text);
+}
+
+function wistia_add_scripts_if_necessary($text) {
+  global $wistia_anti_mangler;
+  return $wistia_anti_mangler->add_scripts_if_necessary($text);
+}
+
+/**
+ * Extends the allowed HTML elements in the TinyMCE editor.
+ *
+ * @link http://bit.ly/11hhyG3
+ *
+ * @param array $init Initial associative array of allowed elements.
+ *
+ * @return array
+ */
+function add_valid_tiny_mce_elements( $init ) {
+	$elements = 'div[*],iframe[*],script[*],object[*],embed[*],a[*],noscript[*]';
+
+	/*
+	Add to extended_valid_elements if it already exists,
+	else ours becomes the only allowed elements.
+	*/
+	if ( isset( $init['extended_valid_elements'] ) && is_string( $init['extended_valid_elements'] ) )
+    $init['extended_valid_elements'] = $init['extended_valid_elements'] . ',' . $elements;
+	else
+		$init['extended_valid_elements'] = $elements;
+
+	return $init;
+}
+
+add_filter('content_save_pre', 'wistia_extract_embeds', 2);
+add_filter('content_save_pre', 'wistia_insert_embeds', 1001);
+add_filter('the_content', 'wistia_extract_embeds', 2);
+add_filter('the_content', 'wistia_insert_embeds', 1001);
+add_filter('the_content', 'wistia_add_scripts_if_necessary', 1002);
+add_filter('the_editor_content', 'wistia_extract_embeds', 2);
+add_filter('the_editor_content', 'wistia_insert_embeds_for_editor', 1001);
+add_filter('tiny_mce_before_init', 'add_valid_tiny_mce_elements' );
+
+?>
