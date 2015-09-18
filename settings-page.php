9576b773419218e772e02bc485652e7f488fefbc
Max Schnur <mschnur@wistia.com>
2015-09-18 09:41:04 -0400

    Tabs to spaces.

--- settings-page.php
+++ settings-page.php
@@ -10,29 +10,29 @@ function wistia_wordpress_add_admin_menu() {
 }
 
 function wistia_wordpress_settings_init(  ) {
-	register_setting('pluginPage', 'wistia_wordpress_settings');
+  register_setting('pluginPage', 'wistia_wordpress_settings');
 
-	add_settings_section(
-		'wistia_wordpress_pluginPage_section',
-		__( '', 'wordpress' ),
-		'wistia_wordpress_settings_section_callback',
-		'pluginPage'
-	);
+  add_settings_section(
+    'wistia_wordpress_pluginPage_section',
+    __( '', 'wordpress' ),
+    'wistia_wordpress_settings_section_callback',
+    'pluginPage'
+  );
 
-	add_settings_field(
-		'anti_mangler',
-		__('Include Legacy Anti-Mangler', 'wordpress'),
-		'wistia_wordpress_anti_mangler_render',
-		'pluginPage',
-		'wistia_wordpress_pluginPage_section'
-	);
+  add_settings_field(
+    'anti_mangler',
+    __('Include Legacy Anti-Mangler', 'wordpress'),
+    'wistia_wordpress_anti_mangler_render',
+    'pluginPage',
+    'wistia_wordpress_pluginPage_section'
+  );
 }
 
 function wistia_wordpress_anti_mangler_render() {
   $wistia_wordpress_anti_mangler = get_wistia_wordpress_option('anti_mangler');
   $on = empty($wistia_wordpress_anti_mangler) ||
     $wistia_wordpress_anti_mangler == 'on';
-	?>
+  ?>
   <label>
     <input type='radio' name='wistia_wordpress_settings[anti_mangler]' <?php echo $on ? "checked='checked'" : ''; ?> value='on'>
     On
@@ -41,24 +41,24 @@ function wistia_wordpress_anti_mangler_render() {
     <input type='radio' name='wistia_wordpress_settings[anti_mangler]' <?php echo $on ? '' : 'checked="checked"'; ?> value='off'>
     Off
   </label>
-	<?php
+  <?php
 }
 
 function wistia_wordpress_settings_section_callback() {
-	echo __('<p>The Wistia WordPress plugin enables oEmbeds to work in WordPress.</p> <p>There is also some legacy functionality which allows raw HTML, which we recommend turning OFF. It\'s better to use oEmbed!</p>', 'wordpress');
+  echo __('<p>The Wistia WordPress plugin enables oEmbeds to work in WordPress.</p> <p>There is also some legacy functionality which allows raw HTML, which we recommend turning OFF. It\'s better to use oEmbed!</p>', 'wordpress');
 }
 
 function wistia_wordpress_options_page() {
-	?>
-	<form action='options.php' method='post'>
-		<h2>Wistia WordPress</h2>
-		<?php
-		settings_fields('pluginPage');
-		do_settings_sections('pluginPage');
-		submit_button();
-		?>
-	</form>
-	<?php
+  ?>
+  <form action='options.php' method='post'>
+    <h2>Wistia WordPress</h2>
+    <?php
+    settings_fields('pluginPage');
+    do_settings_sections('pluginPage');
+    submit_button();
+    ?>
+  </form>
+  <?php
 }
 
 // Link to Settings page from the Plugins page next to Deactivate | Edit.
